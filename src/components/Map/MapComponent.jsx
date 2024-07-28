e2fbf3b5a8d356618e6d3b38dc729158417fe283
Seng Porkeat <alexkgm2412@gmail.com>
2024-07-28 14:07:04 +0700

    project

--- src/components/Map/MapComponent.jsx
+++ src/components/Map/MapComponent.jsx
@@ -18,7 +18,7 @@ const MapComponent = () => {
         let nextUrl = apiUrl;
 
         while (nextUrl) {
-          nextUrl = nextPage.replace(/^http:/, "https:");
+          nextUrl = nextUrl.replace(/^http:/, "https:");
           const response = await fetch(nextUrl);
           const data = await response.json();
           allClubs = [...allClubs, ...data.results];
