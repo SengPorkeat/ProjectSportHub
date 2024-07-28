facb9c1f438c8b333e78a8f8bb177e5cc32ffa3e
Seng Porkeat <alexkgm2412@gmail.com>
2024-07-28 12:58:23 +0700

    project

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "process.env": process.env,
+  },
   server: {
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "https://sporthub.automatex.dev",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   root: ".",
   plugins: [react()],
